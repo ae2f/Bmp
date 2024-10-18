5742a33480f939681bb64cd2b250180e0846b160
Yui San ae2f <mincraft20046@gmail.com>
2024-10-19 00:21:26 +0900

    Update Default.c

--- src/Src/Default.c
+++ src/Src/Default.c
@@ -9,8 +9,6 @@
 
 typedef ae2f_Bmp_cSrc_Copy_ColourIdx(1) ae2f_Bmp_cSrc_BuildPrm_ColourIdx_LeastSuggested_t;
 
-#define SOMETHING_SMALL 1
-
 ae2f_extern ae2f_SHAREDCALL ae2f_errint_t ae2f_Bmp_cSrc_gDot(
 	const ae2f_struct ae2f_Bmp_cSrc* src,
 	uint32_t* retColour,
@@ -19,8 +17,107 @@ ae2f_extern ae2f_SHAREDCALL ae2f_errint_t ae2f_Bmp_cSrc_gDot(
 	double _max_x,
 	double _max_y
 ) {
-	retColour[0] = ae2f_Bmp_Dot_RGBA_Make(_min_x, _min_y, _max_x, 255);
+	if(!(src && retColour && src->Addr))
+	return ae2f_errGlobal_PTR_IS_NULL;
 
+	struct {
+		double R, G, B, A, Count;
+	} Channel = {0, 0, 0, 0, 0};
+
+	switch(src->ElSize) {
+		case ae2f_Bmp_Idxer_eBC_RGB:
+		case ae2f_Bmp_Idxer_eBC_RGBA:
+		break;
+		default: return ae2f_errGlobal_IMP_NOT_FOUND;
+	}
+
+	if(_min_x < 0) _min_x = 0;
+	if(_min_y < 0) _min_y = 0;
+	if(_max_x < 0) _max_x = 0;
+	if(_max_y < 0) _max_y = 0;
+
+	if(_min_x >= ae2f_Bmp_Idx_XLeft(src->rIdxer)) 
+	_min_x = ae2f_Bmp_Idx_XLeft(src->rIdxer);
+
+	if(_max_x >= ae2f_Bmp_Idx_XLeft(src->rIdxer))
+	_max_x = ae2f_Bmp_Idx_XLeft(src->rIdxer);
+
+	if(_min_y >= ae2f_Bmp_Idx_YLeft(src->rIdxer)) 
+	_min_y = ae2f_Bmp_Idx_YLeft(src->rIdxer);
+
+	if(_max_y >= ae2f_Bmp_Idx_YLeft(src->rIdxer)) 
+	_max_y = ae2f_Bmp_Idx_YLeft(src->rIdxer);
+
+	if(_min_x >= _max_x || _min_y >= _max_y) {
+		retColour = 0;
+		return ae2f_errGlobal_OK;
+	}
+
+	struct {
+		size_t Width, Height;
+	} Size;
+	struct {
+		size_t minx, maxx, miny, maxy;
+	} Corner; // as integer
+
+	Corner.maxx = (size_t)_max_x;
+	Corner.maxy = (size_t)_max_y;
+	
+	Corner.minx = (size_t)_min_x;
+	Corner.miny = (size_t)_min_y;
+
+	if(Corner.minx == Corner.maxx)
+	Corner.maxx++;
+
+	if(Corner.miny == Corner.maxy)
+	Corner.maxy++;
+
+	#pragma region Centre
+	for(size_t i = Corner.minx; i < Corner.maxx; i++)
+	for(size_t j = Corner.miny; j < Corner.maxy; j++) {
+		const uint8_t* const __src = src->Addr + ae2f_Bmp_Idx_Drive(src->rIdxer, i, j) * (src->ElSize >> 3);
+
+		if(__src + 1 == src->Addr) continue;
+
+		switch(src->ElSize) {
+			case ae2f_Bmp_Idxer_eBC_RGB: {
+				Channel.R += __src[0];
+				Channel.G += __src[1];
+				Channel.B += __src[2];
+				Channel.Count += 1;
+			} break;
+
+			case ae2f_Bmp_Idxer_eBC_RGBA: {
+				Channel.R += __src[0] * __src[3];
+				Channel.G += __src[1] * __src[3];
+				Channel.B += __src[2] * __src[3];
+				Channel.A += __src[3];
+				Channel.Count += __src[3];
+			} break;
+		}
+	}
+	#pragma endregion
+
+	switch(src->ElSize) {
+		case ae2f_Bmp_Idxer_eBC_RGBA: {
+			retColour[0] = 
+			ae2f_Bmp_Dot_RGBA_Make(
+				Channel.R / Channel.Count, 
+				Channel.G / Channel.Count,
+				Channel.B / Channel.Count,
+				Channel.A / Channel.Count
+			);
+		} break;
+		case ae2f_Bmp_Idxer_eBC_RGB: {
+			retColour[0] = 
+			ae2f_Bmp_Dot_RGBA_Make(
+				Channel.R / Channel.Count,
+				Channel.G / Channel.Count,
+				Channel.B / Channel.Count,
+				255
+			);
+		} break;
+	}
 	return ae2f_errGlobal_OK;
 }
 
@@ -155,7 +252,10 @@ ae2f_SHAREDEXPORT ae2f_errint_t ae2f_Bmp_cSrc_Copy_Partial(
 	uint32_t partial_max_x,
 	uint32_t partial_max_y
 ) {
-		if (!(src && dest && srcprm && src->Addr && dest->Addr)) {
+	#define srcprm ae2f_static_cast(const ae2f_Bmp_cSrc_BuildPrm_ColourIdx_LeastSuggested_t*, _srcprm)
+	ae2f_errint_t code;
+
+	if (!(src && dest && srcprm && src->Addr && dest->Addr)) {
 		return ae2f_errGlobal_PTR_IS_NULL;
 	}
 
@@ -169,96 +269,27 @@ ae2f_SHAREDEXPORT ae2f_errint_t ae2f_Bmp_cSrc_Copy_Partial(
 	default: return ae2f_errGlobal_IMP_NOT_FOUND;
 	}
 
-	uint32_t
-		dotW = ae2f_Bmp_Idx_XLeft(src->rIdxer) / srcprm->global.WidthAsResized,
-		dotH = ae2f_Bmp_Idx_YLeft(src->rIdxer) / srcprm->global.HeightAsResized;
+	
+	double 
+		dotw = (ae2f_Bmp_Idx_XLeft(src->rIdxer) / (double)srcprm->global.WidthAsResized), 
+		doth = (ae2f_Bmp_Idx_YLeft(src->rIdxer) / (double)srcprm->global.HeightAsResized);
 
-	dotW || (dotW++);
-	dotH || (dotH++);
-
-	for (uint32_t y = partial_min_y; y < partial_max_y && y < srcprm->global.HeightAsResized; y++) 
+	for (uint32_t y = partial_min_y; y < partial_max_y && y < srcprm->global.HeightAsResized; y++)
 		for (uint32_t x = partial_min_x; x < partial_max_x && x < srcprm->global.WidthAsResized; x++)
 		{
-			// is it able to be pasted in this dir
-			if (
-				(ae2f_Bmp_Idx_Drive(dest->rIdxer, x + srcprm->global.AddrXForDest, y + srcprm->global.AddrYForDest))
-				== 
-				ae2f_static_cast(uint32_t, -1)
-				) break;
+			union {
+				uint32_t a;
+				uint8_t b[4];
+			} el;
 
-			uint64_t
-				tmpColour_Count = 0,
-				tmpColour[4] = { 0, 0, 0, src->ElSize == ae2f_Bmp_Idxer_eBC_RGBA ? 0 : srcprm->global.Alpha };
+			if(src->ElSize == ae2f_Bmp_Idxer_eBC_RGB) {
+				el.b[3] = srcprm->global.Alpha;
+			}
 
-#define tmpColour_R tmpColour[0]
-#define tmpColour_G tmpColour[1]
-#define tmpColour_B tmpColour[2]
-#define tmpColour_A tmpColour[3]
-
-			for (uint32_t doty = 0; doty < dotH; doty++) 
-				for(uint32_t dotx = 0; dotx < dotW; dotx++) {
-					// index for source as count
-					uint64_t srcDir = ae2f_Bmp_Idx_Drive(src->rIdxer, x * dotW + dotx, y * dotH + doty);
-
-					if (srcDir == -1) break;
-					switch (src->ElSize) {
-					case ae2f_Bmp_Idxer_eBC_RGBA:
-					case ae2f_Bmp_Idxer_eBC_RGB: {
-						uint32_t
-							_tmpColour_R = (src->Addr + (srcDir * (src->ElSize >> 3)))[0],
-							_tmpColour_G = (src->Addr + (srcDir * (src->ElSize >> 3)))[1],
-							_tmpColour_B = (src->Addr + (srcDir * (src->ElSize >> 3)))[2];
-
-						if (
-							ae2f_Bmp_Dot_RGB_Make(_tmpColour_R, _tmpColour_G, _tmpColour_B) == srcprm->global.DataToIgnore
-							&& src->ElSize == ae2f_Bmp_Idxer_eBC_RGB
-							) { break; }
-
-						switch (src->ElSize) {
-						case ae2f_Bmp_Idxer_eBC_RGBA:
-							_tmpColour_R *= (src->Addr + (srcDir * (src->ElSize >> 3)))[3];
-							_tmpColour_G *= (src->Addr + (srcDir * (src->ElSize >> 3)))[3];
-							_tmpColour_B *= (src->Addr + (srcDir * (src->ElSize >> 3)))[3];
-							tmpColour_A += (src->Addr + (srcDir * (src->ElSize >> 3)))[3] * (src->Addr + (srcDir * (src->ElSize >> 3)))[3];
-						case ae2f_Bmp_Idxer_eBC_RGB:
-							tmpColour_R += _tmpColour_R;
-							tmpColour_G += _tmpColour_G;
-							tmpColour_B += _tmpColour_B;
-							break;
-						}
-
-						tmpColour_Count += src->ElSize == ae2f_Bmp_Idxer_eBC_RGBA ? (src->Addr + (srcDir * (src->ElSize >> 3)))[3] : 1;
-					} break;
-
-					default: { // 1 4 8
-						uint8_t ColourIdx 
-							= ae2f_Macro_BitVector_GetRanged(
-								(src->Addr + ((srcDir * src->ElSize) >> 3))[0],
-								(srcDir * src->ElSize) & 7,
-								((srcDir * src->ElSize) & 7) + src->ElSize
-							);
-
-						uint32_t ColourTmp = srcprm->ColourIdx[ColourIdx];
-
-						if (ColourTmp == srcprm->global.DataToIgnore) {
-							tmpColour_Count++;
-							break;
-						}
-
-						tmpColour_R += ae2f_Bmp_Dot_RGBA_GetR(ColourTmp);
-						tmpColour_G += ae2f_Bmp_Dot_RGBA_GetG(ColourTmp);
-						tmpColour_B += ae2f_Bmp_Dot_RGBA_GetB(ColourTmp);
-						tmpColour_Count++;
-					} break;
-					}
-				}
-
-			if(!tmpColour_Count) continue;
-			tmpColour_R /= tmpColour_Count;
-			tmpColour_G /= tmpColour_Count;
-			tmpColour_B /= tmpColour_Count;
-
-			(src->ElSize == ae2f_Bmp_Idxer_eBC_RGBA) && (tmpColour_A /= tmpColour_Count);
+			code = ae2f_Bmp_cSrc_gDot(src, &el.a, dotw * x, doth * y, dotw * (x + 1), doth * (y+1));
+			if(code != ae2f_errGlobal_OK) {
+				return code;
+			}
 
 			for (
 				uint8_t* 
@@ -274,21 +305,18 @@ ae2f_SHAREDEXPORT ae2f_errint_t ae2f_Bmp_cSrc_Copy_Partial(
 				
 				switch (i) {
 				default: {
-
 					addr[i] = ae2f_Bmp_Dot_Blend_imp(
-						tmpColour[i], 
+						el.b[i], 
 						addr[i], 
-						((ae2f_static_cast(double, tmpColour_A)) / ae2f_static_cast(double, addr_a + tmpColour_A)), 
+						((ae2f_static_cast(double, el.b[3])) / ae2f_static_cast(double, addr_a + el.b[3])), 
 						uint8_t, 
 					);
 				} break;
 				case 3: {
-					addr[i] = (ae2f_static_cast(uint16_t, addr[i]) + tmpColour[i]) >> 1;
+					addr[i] = (ae2f_static_cast(uint16_t, addr[i]) + el.b[i]) >> 1;
 				}
 				}
 			}
-#undef Done
-#undef addr_tar
 		}
 	return ae2f_errGlobal_OK;
 }
