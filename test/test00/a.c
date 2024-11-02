f16e74a6ec4e8e29344efc0066031ab046ed29a8
Yui San ae2f <mincraft20046@gmail.com>
2024-11-02 15:55:36 +0900

    Test Update

--- test/test00/a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <ae2f/Bmp/Dot.h>
-#include <ae2f/Bmp/Idxer.h>
-
-int main() {
-    return 
-    (snprintf(0, 0, ae2f_Bmp_Dot_h) ^ 2810) ||
-    (snprintf(0, 0, ae2f_Bmp_Idxer_h) ^ 1086);
-}
